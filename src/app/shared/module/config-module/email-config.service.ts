4a23e4a79761451698f0e9924d084d2ada9941b2
Nayf <nayfserag5@gmail.com>
2024-10-18 23:40:06 +0300

    make endpoint is generic

--- src/app/shared/module/config-module/email-config.service.ts
+++ src/app/shared/module/config-module/email-config.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
+import { config } from './config.service';
 
 @Injectable()
 export class EmailConfigService {
   createTransporter(): nodemailer.Transporter {
     return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST || 'smtp.gmail.com',  // Use environment variables for security
-      port: Number(process.env.EMAIL_PORT) || 587,
+      host: config.getString('EMAIL_HOST'), 
+      port: config.getNumber('EMAIL_PORT'),
       secure: false, // true for 465, false for other ports
       auth: {
-        user: process.env.EMAIL_USER || 'expromentalprojects@gmail.com', // Your email
-        pass: process.env.EMAIL_PASSWORD || 'tgws xkrd zusg ieha', // Your password
+        user: config.getString('EMAIL_USER'),
+        pass: config.getString('EMAIL_PASSWORD'), 
       },
     });
   }
